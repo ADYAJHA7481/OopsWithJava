bdc53c4b9631fdc638829ed18d4857ee4279d403
Adya <adyajha776@gmail.com>
2024-03-11 21:13:31 +0530

    Update code

--- lab2/ex1.java
+++ lab2/ex1.java
@@ -11,39 +11,42 @@ public class ex1 {
     {
         // Using Command Line
 
-        /*int a = Integer.parseInt(args[0]);
+        int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
         int c = a+b;
         System.out.println("First Number = "+a);
         System.out.println("Second Number = "+b);
-        System.out.println("Sum = "+c); */
+        System.out.println("Sum = "+c); 
 
         // Using Buffer Reader
 
-        /*try {
-            BufferedReader a = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            BufferedReader d = new BufferedReader(new InputStreamReader(System.in));
             System.out.print("Enter first number : ");
-            int num1 = Integer.parseInt(a.readLine());
+            int num1 = Integer.parseInt(d.readLine());
             System.out.print("Enter second number : ");
-            int num2 = Integer.parseInt(a.readLine());
+            int num2 = Integer.parseInt(d.readLine());
             int sum = num1 + num2;
             System.out.println("Sum : " + sum);
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
+        }
 
+        //Using DataInputStream
         
-        /*try {
-            DataInputStream a = new DataInputStream(System.in);
+        try {
+            DataInputStream e = new DataInputStream(System.in);
             System.out.print("Enter first number : ");
-            int num1 = Integer.parseInt(a.readLine());
+            int num1 = Integer.parseInt(e.readLine());
             System.out.print("Enter second number : ");
-            int num2 = Integer.parseInt(a.readLine());
+            int num2 = Integer.parseInt(e.readLine());
             int sum = num1 + num2;
             System.out.println("Sum : " + sum);
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
+        }
+
+        //Using Scanner Class
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter first number : ");
@@ -51,7 +54,7 @@ public class ex1 {
         System.out.print("Enter second number : ");
         int num2 = sc.nextInt();
         int sum = num1 + num2;
-        System.out.println("Sum using Scanner: " + sum);
+        System.out.println("Sum : " + sum);
         sc.close();
     
     }
