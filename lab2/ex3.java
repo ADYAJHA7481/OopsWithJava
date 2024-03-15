830feea4a57723eb7c8545a7652f08a2c528c4c0
Adya <adyajha776@gmail.com>
2024-03-16 00:10:14 +0530

    User input in array to find largest and smallest

--- lab2/ex3.java
+++ lab2/ex3.java
@@ -1,15 +1,16 @@
 //Write a program to find largest and smallest number in an array   
+import java.util.*;
 public class ex3 {
-    public static void small_large(int arr[])
+    public static void small_large(int arr[],int size)
     {   int max = arr[0];
         int min = arr[0];
-        for(int i=0; i<arr.length; i++)
+        for(int i=0; i<size; i++)
         {
             if(arr[i]>max)
             {
                 max = arr[i];
             }
-            if(arr[i]<=min)
+            if(arr[i]<min)
             {
                 min = arr[i];
             }
@@ -19,7 +20,16 @@ public class ex3 {
     }
     public static void main(String args[])
     {
-        int arr[] = {1,5,6,7,18,11,2};        
-        small_large(arr);
+        int i,size;
+        int a[]=new int [20];
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter size of array : ");
+        size = sc.nextInt();
+        System.out.println("Enter the elements of array : ");
+        for(i=0;i<size;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        small_large(a, size);
     }
 }
