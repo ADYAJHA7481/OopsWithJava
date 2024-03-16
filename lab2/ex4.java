48f811fc1fae2e30f2a8632d00c6ac88f4e1e3c1
Adya <adyajha776@gmail.com>
2024-03-16 16:54:17 +0530

    program to add matrices

--- lab2/ex4.java
+++ lab2/ex4.java
@@ -1,4 +1,45 @@
-//Write a program to add two matrices.
+//Write a program to add  matrices.
+import java.util.*;
 public class ex4 {
-    
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int row , coloum ;
+        System.out.print("Enter Row : ");
+        row = sc.nextInt();
+        System.out.print("Enter coloum : ");
+        coloum = sc.nextInt();
+        int i ,j ;
+        int a[][]=new int[20][20];
+        int b[][]=new int[20][20];
+        System.out.println("Enter elements of first matrix  ");
+        for(i=1;i<=row;i++)
+        {
+            for(j=1;j<=coloum;j++)
+            {
+                System.out.print("Enter number for "+i + ""+ j + " : " );
+                a[i][j]=sc.nextInt();
+            }
+        }       
+
+        System.out.println("Enter elements of Second matrix  ");
+        
+        for(i=1;i<=row;i++)
+        {
+            for(j=1;j<=coloum;j++)
+            {
+                System.out.print("Enter number for "+i + ""+ j + ": ");
+                b[i][j]=sc.nextInt();
+            }
+        }  
+         
+        for(i=1;i<=row;i++)
+        {
+            for(j=1;j<=coloum;j++)
+            {
+                System.out.print(a[i][j]+b[i][j]+" ");
+            }
+            System.out.println("");
+        }  
+    }
 }
