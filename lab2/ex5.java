1e63242a0d6454362f00b019ef8bfd0312132905
Adya <adyajha776@gmail.com>
2024-03-16 17:08:55 +0530

    Create ex5

---
+++ lab2/ex5.java
@@ -0,0 +1,7 @@
+//Write a program to search an element in an array 
+import java.util.* ;
+public class ex5 {
+    int a[] = new int[30];
+    int size ;
+    Scanner sc = new Scanner(System.in);
+}
