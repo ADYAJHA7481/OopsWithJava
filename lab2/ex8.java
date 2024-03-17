f029be53d7e3b871d5ebbe5ad9dda5525f9377e0
Adya <adyajha776@gmail.com>
2024-03-18 00:25:47 +0530

    sort elements of 1-D array

---
+++ lab2/ex8.java
@@ -0,0 +1,34 @@
+//Write a program to sort all the elements of 1-D array.
+import java.util.Scanner;
+public class ex8 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        // Print the sorted array
+        System.out.println("Sorted array:");
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
