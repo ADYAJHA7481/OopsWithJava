9c8c3515161e401f9c264642cc1dd49f30a8b8a9
Adya <adyajha776@gmail.com>
2024-04-27 12:20:16 +0530

    WAP  to take input from user to print name and age & compare the age by creating objects and methods in two different classes

--- lab3/ex5.java
+++ lab3/ex5.java
@@ -1,33 +1,57 @@
 //WAP  to take input from user to print name and age & compare the age by creating objects and methods in two different classes. 
 import java.util.*;
 
-class Name_age
-{
-    public void Name()
-    {
-        String name;
-        int age;
-        Scanner sc =new Scanner(System.in);
-        System.out.print("Enter Your name : ");
-        name = sc.nextLine();
-        System.out.print("Enter Your age : ");
-        age= sc.nextInt();
-        System.out.print("Your Name is "+name);
-        System.out.print("\nYour age is "+age);
-    }
-};
+class Name_age {
+    private String name;
+    private int age;
 
-class CompAge
-{
-    public void CompareAge(Name_age user1,Name_age user2)
-    {
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+class CompAge {
+    public void compAge(Name_age user1, Name_age user2) {
+        if (user1.getAge() > user2.getAge()) {
+            System.out.println(user1.getName() + " is older than " + user2.getName());
+        } else if (user1.getAge() < user2.getAge()) {
+            System.out.println(user2.getName() + " is older than " + user1.getName());
+        } else {
+            System.out.println(user1.getName() + " and " + user2.getName() + " are of the same age.");
+        }
     }
 }
 
 public class ex5 {
-    
     public static void main(String[] args) {
-        Name_age user1=new Name_age();
-        user1.Name();
+        Scanner sc = new Scanner(System.in);
+        
+        Name_age user1 = new Name_age();
+        System.out.print("Enter Your name : ");
+        user1.setName(sc.nextLine());
+        System.out.print("Enter Your age : ");
+        user1.setAge(sc.nextInt());
+
+        Name_age user2 = new Name_age();
+        System.out.print("Enter Your name : ");
+        sc.nextLine();
+        user2.setName(sc.nextLine());
+        System.out.print("Enter Your age : ");
+        user2.setAge(sc.nextInt());
+
+        CompAge c1 = new CompAge();
+        c1.compAge(user1, user2);
     }
 }
